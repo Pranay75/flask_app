eaee5e9ca103381481d418e71a2d4e9d1cd326f9
Pranay Chalimeti <pranaychalimeti@gmail.com>
2024-07-26 11:22:29 +0530

    added install scripts

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+sudo apt install nginx
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+# Systemd Service
+echo "Creating systemd service..."
+sudo tee /etc/systemd/system/question-panel-app.service << EOF
+[Service]
+User=$USER
+Group=www-data
+WorkingDirectory=$PWD
+Environment="PATH=$PWD/venv/bin"
+ExecStart=$PWD/venv/bin/gunicorn -w 3 --bind unix:app.sock -m 007 wsgi:app
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable app
+# Nginx config
+echo "Creating nginx server..."
+sudo tee /etc/nginx/sites-available/question-panel-app << EOF
+server {
+listen 80;
+server_name question-panel;
+
+location / {
+  include proxy_params;
+  proxy_pass http://unix:$PWD/app.sock;
+  proxy_set_header Host \$http_host;
+  proxy_set_header X-Real-IP \$remote_addr;
+  proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+  proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+
+}
+EOF
+
+
+sudo ln -s /etc/nginx/sites-available/question-panel-app /etc/nginx/sites-enabled
+sudo chmod 775 -R ./
+sudo chmod 775 -R ../
+sudo nginx -t && sudo nginx -s reload && \
+sudo systemctl restart nginx
---
+++ run.sh
@@ -0,0 +1,3 @@
+sudo systemctl restart question-panel-app
+sudo service question-panel-app restart
+sudo service nginx restart
